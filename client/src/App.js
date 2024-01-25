e2e34634aeabb0dc1c29e38e9be5c03e63181dee
Jezerig <jesse_pasanen@hotmail.com>
2024-01-25 04:13:01 +0200

    top bar works on all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Members from './components/Members';
-
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <TopBar />
         <Routes>
         <Route path="/members" element={<Members />} />
         </Routes>
