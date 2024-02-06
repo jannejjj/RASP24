d27a2a4517b4e395e661ea8c8838b344f956ae1d
Miro Hakuli <miro@HackBookPro.local>
2024-02-06 11:49:25 +0200

    myprofile: front

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,9 +1,220 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import '../styles/MyProfile.css';
+import Button from "@mui/material/Button";
+import EditDetailsModal from '../modals/EditDetailsModal';
 
-function MyProfile() {
+
+function EventItem(props)
+{
+
+  // These states store the original event data
+  const [Firstname, setFirstname] = useState(props.Firstname);
+  const [Lastname, setLastname] = useState(props.Lastname);
+  const [Phone, setPhone] = useState(props.Phone);
+  const [Address, setAddress] = useState(props.Address);
+  const [Postalcode, setPostalcode] = useState(props.Postalcode);
+  const [City, setCity] = useState(props.City);
+  const [Country, setCountry] = useState(props.Country);
+  const [Email, setEmail] = useState(props.Email);
+
+  // These states store the data that is edited
+  const [edit, setEdit] = useState(false);
+  const [editedFirstname, setEditedFirstname] = useState(props.Firstname);
+  const [editedLastname, setEditedLastname] = useState(props.Lastname);
+  const [editedPhone, setEditedPhone] = useState(props.Phone);
+  const [editedAddress, setEditedAddress] = useState(props.Address);
+  const [editedPostalcode, setEditedPostalcode] = useState(props.Postalcode);
+  const [editedCity, setEditedCity] = useState(props.City);
+  const [editedCountry, setEditedCountry] = useState(props.Country);
+  const [editedEmail, setEditedEmail] = useState(props.Email);
+
+  // Save the history so that the editing can be cancelled
+  const [FirstnameHistory, setFirstnameHistory] = useState(props.Firstname);
+  const [LastnameHistory, setLastnameHistory] = useState(props.Lastname);
+  const [PhoneHistory, setPhoneHistory] = useState(props.Phone);
+  const [AddressHistory, setAddressHistory] = useState(props.Address);
+  const [PostalcodeHistory, setPostalcodeHistory] = useState(props.Postalcode);
+  const [CityHistory, setCityHistory] = useState(props.City);
+  const [CountryHistory, setCountryHistory] = useState(props.Country);
+  const [EmailHistory, setEmailHistory] = useState(props.Email);
+
+  const editOnClick = () =>
+  {
+    setEdit(true);
+  }
+
+  const saveEditOnClick = () =>
+  {
+    // TODO: Send the edited values to the database to actually save the edit
+
+
+
+    // Update the history
+    setFirstnameHistory(editedFirstname);
+    setLastnameHistory(editedLastname);
+    setPhoneHistory(editedPhone);
+    setAddressHistory(editedAddress);
+    setPostalcodeHistory(editedPostalcode);
+    setCityHistory(editedCity);
+    setCountryHistory(editedCountry);
+    setEmailHistory(editedEmail);
+
+    // Update the actual values
+    setFirstname(editedFirstname);
+    setLastname(editedLastname);
+    setPhone(editedPhone);
+    setAddress(editedAddress);
+    setPostalcode(editedPostalcode);
+    setCity(editedCity);
+    setCountry(editedCountry);
+    setEmail(editedEmail);
+
+    // Close the edit
+    setEdit(false);
+  }
+
+  const cancelEditOnClick = () =>
+  {
+    // Bring back the old event data
+    setFirstname(FirstnameHistory);
+    setLastname(LastnameHistory);
+    setPhone(PhoneHistory);
+    setAddress(AddressHistory);
+    setPostalcode(PostalcodeHistory);
+    setCity(CityHistory);
+    setCountry(CountryHistory);
+    setEmail(EmailHistory);
+
+    // Also reset the changes to the edited values
+    setEditedFirstname(FirstnameHistory);
+    setEditedLastname(LastnameHistory);
+    setEditedPhone(PhoneHistory);
+    setEditedAddress(AddressHistory);
+    setEditedPostalcode(PostalcodeHistory);
+    setEditedCity(CityHistory);
+    setEditedCountry(CountryHistory);
+    setEditedEmail(EmailHistory);
+
+    // Close the edit
+    setEdit(false);
+  }
+
+  const handleFirstnameChange = (event) =>
+  {
+    setEditedFirstname(event.target.value);
+  }
+
+  const handleLastnameChange = (event) =>
+  {
+    setEditedLastname(event.target.value);
+  }
+
+  const handlePhoneChange = (event) =>
+  {
+    setEditedPhone(event.target.value);
+  }
+
+  const handleAddressChange = (event) =>
+  {
+    setEditedAddress(event.target.value);
+  }
+
+  const handlePostalcodeChange = (event) =>
+  {
+    setEditedPostalcode(event.target.value);
+  }
+
+  const handleCityChange = (event) =>
+  {
+    setEditedCity(event.target.value);
+  }
+
+  const handleCountryChange = (event) =>
+  {
+    setEditedCountry(event.target.value);
+  }
+
+  const handleEmailChange = (event) =>
+  {
+    setEditedEmail(event.target.value);
+  }
+
   return (
-    <div>MyProfile</div>
+    <div className='MyInfo'>
+      <img src='https://blogs.lut.fi/newcomers/wp-content/uploads/sites/15/2020/02/talvi-ilma-1-1.jpg' />
+      <div>
+        <p>Firstname: {Firstname}</p>
+        <p>Lastname: {Lastname}</p>
+        <p>Phone: {Phone}</p>
+        <p>Address: {Address}</p>
+        <p>Postalcode: {Postalcode}</p>
+        <p>City: {City}</p>
+        <p>Country: {Country}</p>
+      </div>
+      <Button variant='outlined' onClick={editOnClick} >Edit</Button>
+
+    <EditDetailsModal 
+      edit={edit} 
+      editedEmail={editedEmail} 
+      editedCountry={editedCountry}
+      editedCity={editedCity}
+      editedFirstname={editedFirstname}
+      editedLastname={editedLastname}
+      editedPhone={editedPhone}
+      editedAddress={editedAddress}
+      editedPostalcode={editedPostalcode}
+      handleFirstnameChange={handleFirstnameChange}
+      handleLastnameChange={handleLastnameChange}
+      handlePhoneChange={handlePhoneChange}
+      handleAddressChange={handleAddressChange}
+      handlePostalcodeChange={handlePostalcodeChange}
+      handleCityChange={handleCityChange}
+      handleCountryChange={handleCountryChange}
+      handleEmailChange={handleEmailChange}
+      cancelEditOnClick={cancelEditOnClick}
+      saveEditOnClick={saveEditOnClick}
+    />
+    </div>
   )
 }
 
-export default MyProfile
+function Home() {
+  const [events, setEvents] = useState([
+    {
+      Firstname: "Jarmo",
+      Lastname: "juuu",
+      Phone: "1234567890",
+      Address: "Jossain",
+      Postalcode: "34567",
+      City: "Lappeenranta",
+      Country: "Finland",
+      Email: `moi@moi.fi`
+    }
+  ])
+
+  return (
+    <div className='HomePageBackground'>
+      <h1>My Profile</h1>
+      <div className='Background'>
+        {events.map((event, index) => (
+          <EventItem
+            Firstname={event.Firstname}
+            Lastname={event.Lastname}
+            Phone={event.Phone}
+            Address={event.Address}
+            Postalcode={event.Postalcode}
+            City={event.City}
+            Country={event.Country}
+            Email= {event.Email}
+            key={index}
+          />
+        ))}
+      </div>
+
+      
+    </div>
+
+  )
+}
+
+export default Home
--- client/src/modals/EditDetailsModal.js
+++ client/src/modals/EditDetailsModal.js
@@ -12,12 +12,17 @@ function EditDetailsModal(props)
       <Modal open={props.edit} >
         <Box className='ModalBackground' >
           <h2>
-            Edit the details
+            Edit profile information
           </h2>
           <div className='HorizontalSeparator' />
-          <TextField value={props.title} onInput={props.handleTitleChange} className='TextField' label="Title" />
-          <TextField multiline value={props.details} onInput={props.handleDetailsChange}
-            className='TextField' fullWidth label='Description' maxRows={15} />
+          <TextField value={props.editedFirstname} onInput={props.handleFirstnameChange} className='TextField' fullWidth label="Firstname" />
+          <TextField value={props.editedLastname} onInput={props.handleLastnameChange} className='TextField' fullWidth label="Lastname" />
+          <TextField value={props.editedPhone} onInput={props.handlePhoneChange} className='TextField' fullWidth label="Phone" />
+          <TextField value={props.editedAddress} onInput={props.handleAddressChange} className='TextField' fullWidth label="Address" />
+          <TextField value={props.editedPostalcode} onInput={props.handlePostalcodeChange} className='TextField' fullWidth label="Postalcode" />
+          <TextField value={props.editedCity} onInput={props.handleCityChange} className='TextField' fullWidth label="City" />
+          <TextField value={props.editedCountry} onInput={props.handleCountryChange} className='TextField' fullWidth label="Country" />
+          <TextField value={props.editedEmail} onInput={props.handleEmailChange} className='TextField' fullWidth label="Email" />
           <div>
             <Button style={{margin: "10px 5px 0 0"}} color='primary' variant='outlined' fullWidth onClick={props.cancelEditOnClick} >Cancel</Button>
             <Button style={{margin: "10px 0 0 5px"}} color='success' variant='outlined' fullWidth onClick={props.saveEditOnClick} >Save</Button>
