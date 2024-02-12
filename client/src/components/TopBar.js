955dfae3cab90139eb195720bc570e5dd5de4f63
KabirLut <157789743+KabirLut@users.noreply.github.com>
2024-02-12 15:09:59 +0200

    log out notification update

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -13,6 +13,8 @@ import { Menu, MenuItem } from "@mui/material";
 import { Link as RouterLink } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
+import Swal from 'sweetalert2';
+
 export default function TopBar() {
   const [language, setLanguage] = useState("EN");
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -43,7 +45,13 @@ export default function TopBar() {
     if(loggedIn) {
       sessionStorage.removeItem('token');
       loggedIn = false;
-      navigate('/Home')
+      navigate('/Home');
+       Swal.fire({
+      icon: 'warning',
+      title: 'Logged out successfully!',
+      showConfirmButton: false,
+      timer: 3000 // Auto close after 1.5 seconds
+    });
     }
   };
 
