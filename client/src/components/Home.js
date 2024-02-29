39d07fd014319d523fff22f1fc0962f423fcccf9
jannejjj <61980297+jannejjj@users.noreply.github.com>
2024-02-29 17:03:38 +0200

    EventItem to its own file, updateEvents toggle into a function, removed commented out events, added a success toast, added TicketsSold to events

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,11 +3,9 @@ import "../styles/HomePage.css";
 import "../App.css";
 import Button from "@mui/material/Button";
 import { FaUserGroup } from "react-icons/fa6";
-import CancelAttendanceModal from "../modals/CancelAttendanceModal";
-import ConfirmAttendanceModal from "../modals/ConfirmAttendanceModal";
 import CreateEventModal from "../modals/CreateEventModal";
-import EditEventModal from "../modals/EditEventModal";
 import EditDetailsModal from "../modals/EditDetailsModal";
+import EventItem from "./EventItem";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Typography from "@mui/material/Typography";
@@ -88,185 +86,7 @@ function Details(props) {
   );
 }
 
-function EventItem(props) {
-  // These states store the original event data
-  const [attending, setAttending] = useState(props.attending);
-  const [title, setTitle] = useState(props.title);
-  const [time, setTime] = useState(props.time);
-  const [location, setLocation] = useState(props.location);
-  const [description, setDescription] = useState(props.description);
-  const [attendees, setAttendees] = useState(props.attendees);
 
-  // These states store the data that is edited
-  const [edit, setEdit] = useState(false);
-  const [openAttend, setOpenAttend] = useState(false);
-  const [openCancelAttendance, setOpenCancelAttendance] = useState(false);
-  const [editedTitle, setEditedTitle] = useState(props.title);
-  const [editedTime, setEditedTime] = useState(props.time);
-  const [editedLocation, setEditedLocation] = useState(props.location);
-  const [editedDescription, setEditedDescription] = useState(props.description);
-
-  // Save the history so that the editing can be cancelled
-  const [titleHistory, setTitleHistory] = useState(props.title);
-  const [timeHistory, setTimeHistory] = useState(props.time);
-  const [locationHistory, setLocationHistory] = useState(props.location);
-  const [descriptionHistory, setDescriptionHistory] = useState(
-    props.description
-  );
-
-  const editOnClick = () => {
-    setEdit(true);
-  };
-
-  const saveEditOnClick = () => {
-    // TODO: Send the edited values to the database to actually save the edit
-
-    // Update the history
-    setTitleHistory(editedTitle);
-    setTimeHistory(editedTime);
-    setLocationHistory(editedLocation);
-    setDescriptionHistory(editedDescription);
-
-    // Update the actual values
-    setTitle(editedTitle);
-    setTime(editedTime);
-    setLocation(editedLocation);
-    setDescription(editedDescription);
-
-    // Close the edit
-    setEdit(false);
-  };
-
-  const cancelEditOnClick = () => {
-    // Bring back the old event data
-    setTitle(titleHistory);
-    setTime(timeHistory);
-    setLocation(locationHistory);
-    setDescription(descriptionHistory);
-
-    // Also reset the changes to the edited values
-    setEditedTitle(titleHistory);
-    setEditedTime(timeHistory);
-    setEditedLocation(locationHistory);
-    setEditedDescription(descriptionHistory);
-
-    // Close the edit
-    setEdit(false);
-  };
-
-  const handleTitleChange = (event) => {
-    setEditedTitle(event.target.value);
-  };
-
-  const handleTimeChange = (event) => {
-    setEditedTime(event.target.value);
-  };
-
-  const handleLocationChange = (event) => {
-    setEditedLocation(event.target.value);
-  };
-
-  const handleDescriptionChange = (event) => {
-    setEditedDescription(event.target.value);
-  };
-
-  const handleEventAttendance = () => {
-    setOpenAttend(false);
-    setAttending(true);
-    setAttendees(attendees + 1);
-  };
-
-  const handleCancelEventAttendance = () => {
-    setOpenCancelAttendance(false);
-    setAttending(false);
-    setAttendees(attendees - 1);
-  };
-  if(props.loggedIn) {
-    return (
-      <div className="HomeEventItem">
-        <div className="HomeEventTop">
-          <div className="HomeEventPhotoBackground">
-            <img
-              src="https://blogs.lut.fi/newcomers/wp-content/uploads/sites/15/2020/02/talvi-ilma-1-1.jpg"
-              alt="Event"
-            />
-          </div>
-  
-          <div className="HomeEventHeaderArea">
-            <div className="HomeEventTitleAndLocationArea">
-              <h2>{title}</h2>
-              <h3>Created by: {props.creator}</h3>
-              <h3>{time}</h3>
-              <h3>{location}</h3>
-            </div>
-  
-            <p>
-              <FaUserGroup style={{ margin: "0 5px 0 0" }} /> {attendees}
-            </p>
-          </div>
-        </div>
-  
-        <div className="HomeEventBottom">
-          <div className="HorizontalSeparator" />
-          <div className="HomeEventDescriptionArea">
-            <p>{description}</p>
-          </div>
-  
-          <div className='HomeEventAttendanceButtonsArea'>
-            {props.admin && 
-              (
-                <Button className='EditEventButton' variant='contained' onClick={editOnClick} >Edit</Button>
-              )
-            }
-            <div>
-              {attending ? 
-                (
-                  <Button variant='outlined' color='primary' onClick={() => {setOpenCancelAttendance(true)}} >Cancel Attendance</Button>
-                )
-                :
-                (
-                  <Button variant='contained' color='primary' onClick={() => {setOpenAttend(true)}} >Attend the Event</Button>
-                )
-              }
-            </div>
-          </div>
-        </div>
-  
-        <EditEventModal
-          edit={edit}
-          editedDescription={editedDescription}
-          editedLocation={editedLocation}
-          editedTime={editedTime}
-          editedTitle={editedTitle}
-          handleTitleChange={handleTitleChange}
-          handleTimeChange={handleTimeChange}
-          handleLocationChange={handleLocationChange}
-          handleDescriptionChange={handleDescriptionChange}
-          cancelEditOnClick={cancelEditOnClick}
-          saveEditOnClick={saveEditOnClick}
-        />
-  
-        <ConfirmAttendanceModal
-          openAttend={openAttend}
-          setOpenAttend={setOpenAttend}
-          handleEventAttendance={handleEventAttendance}
-        />
-  
-        <CancelAttendanceModal
-          openCancelAttendance={openCancelAttendance}
-          setOpenCancelAttendance={setOpenCancelAttendance}
-          handleCancelEventAttendance={handleCancelEventAttendance}
-        />
-      </div>
-    );
-  } else {
-    return (
-      <Typography sx={{ mt: 20 }} variant='h4' align="center">
-         Please login to view events.
-      </Typography>
-    );
-  }
-  }
   
 
 function Home(props) {
@@ -282,37 +102,11 @@ function Home(props) {
   const [loading, setLoading] = useState(true);
   const [updateEvents, setUpdateEvents] = useState(false);
   const [events, setEvents] = useState([{}]);
-  /*
-  const [events, setEvents] = useState([
-    {
-      title: "TechSynergy Summit",
-      creator: "Emily Thompson",
-      time: "26.2.2024 12:00",
-      location: "LUT University",
-      attendees: 41,
-      attending: true,
-      description: `The TechSynergy Summit is a premier corporate event designed to bring together industry leaders, visionaries, and innovators in the ever-evolving landscape of technology. This exclusive summit serves as a dynamic platform or collaboration, knowledge exchange, and networking. Attendees can expect insightful keynote presentations, interactive panel discussions, and hands-on workshops that explore the intersection of cutting-edge technologies, fostering an environment where ideas converge, and innovation thrives. Join us at TechSynergy Summit to be at the forefront of the technological revolution and cultivate meaningful connections that propel your organization into the future.`,
-    },
-    {
-      title: "FutureTech Showcase",
-      creator: "Liam Patel",
-      time: "2.4.2024 14:00",
-      location: "LUT University",
-      attendees: 24,
-      attending: false,
-      description: `Step into the future with FutureTech Showcase, where cutting-edge innovations and breakthrough technologies converge. Explore the latest advancements in robotics, artificial intelligence, and beyond. Immerse yourself in a curated exhibition that unveils tomorrow's tech landscape today.`,
-    },
-    {
-      title: "Digital Nexus Symposium",
-      creator: "Sophia Mitchell",
-      time: "18.7.2024 10:00",
-      location: "LUT University",
-      attendees: 98,
-      attending: false,
-      description: `Join thought leaders and industry experts at the Digital Nexus Symposium, a dynamic gathering that explores the interconnected world of digital technologies. Engage in insightful discussions on the impact of AI, data analytics, and cyber-physical systems. Discover the converging points shaping our digital future at this symposium of ideas and collaboration.`,
-    },
-  ]);
-  */
+
+  const toggleUpdateEvents = () => {
+    setUpdateEvents(!updateEvents);
+  }
+
   const showToastMessage = (message) =>
     {
         toast.error(message, {
@@ -325,6 +119,19 @@ function Home(props) {
             progress: undefined,
             theme: "dark"
             });
+    }
+  
+    const showToastSuccessMessage = (message) =>  {
+      toast.success(message, {
+          position: "top-center",
+          autoClose: 6000,
+          hideProgressBar: true,
+          closeOnClick: false,
+          pauseOnHover: false,
+          draggable: false,
+          progress: undefined,
+          theme: "dark"
+          });
     }
 
   //Resets the amount of tickets and clears the text box.
@@ -456,7 +263,7 @@ function Home(props) {
           // Close the Modal
           setNewEventModal(false);
           // Update events list by toggling the boolean
-          setUpdateEvents(!updateEvents);
+          toggleUpdateEvents();
           if(checkedTicket) {
             resetTickets();
           }
@@ -490,14 +297,18 @@ function Home(props) {
       <div className="HomeEventsList">
         <h1>Events</h1>
         <div className="HorizontalSeparator" style={{ width: "95%" }} />
+        
         {admin && (
           <Button color="primary" variant='contained' onClick={() => {setNewEventModal(true)}} style={{margin: "10px 0 10px 0"}} >Add New Event</Button>
         )}
+
         {loading && <Typography sx={{ mt: 20 }} variant='h4' align="center">
             Loading...
           </Typography>}
+
         {!loading && events.map((event, index) => (
           <EventItem
+            id={event._id}
             admin={admin}
             loggedIn={props.currentUser.loggedIn}
             title={event.title}
@@ -507,9 +318,15 @@ function Home(props) {
             attendees={event.attendees}
             attending={event.attending}
             description={event.description}
+            ticketsSold={event.ticketsSold}
             key={index}
+            showToastMessage={showToastMessage}
+            showToastSuccessMessage={showToastSuccessMessage}
+            token={props.currentUser.token}
+            toggleUpdateEvents={toggleUpdateEvents}
           />
         ))}
+
         <Typography sx={{ mt: 20 }} variant='h4' align="center">{!events?.length>0 && "No events."}</Typography>
       </div>
 
