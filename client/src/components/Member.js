a7df90ba66110f487794536a1d03145b38f47d63
Nesssu <nessu.kangashaka@gmail.com>
2024-03-13 19:58:34 +0200

    408 Roles for the members: First changes to the member item

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -5,21 +5,34 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { Grid } from '@mui/material';
 
 function Member({member}) {
 
   const card = (
     <React.Fragment >
-        <CardContent sx={{ borderRadius: 2, border: '1.5px solid black' }} >
-            <Typography align='left' variant="h4" component="div" style={{cursor: "pointer"}}>
-              {member.firstname} {member.lastname}
-            </Typography>
-            <Typography align='left' sx={{ mb: 1.5 }} color="text.secondary" style={{cursor: "pointer"}}>
-              {member.email}
-            </Typography>
-            <Stack direction="row" justifyContent="space-between" spacing={5}>
-                <Button  sx={{ maxWidth: '110px', maxHeight: '40px', minWidth: '110px', minHeight: '40px'}}  size="medium" variant="contained">Manage</Button>
-            </Stack>
+        <CardContent sx={{ border: 'none' }} >
+          <Grid container>
+            {/* The profile picture area */}
+            <Grid item>
+
+            </Grid>
+            {/* The memebr info area */}
+            <Grid item>
+              <Typography align='left' variant="h5" component="div" style={{cursor: "pointer"}}>
+                {member.firstname} {member.lastname} <span>- Role</span>
+              </Typography>
+              <Typography align='left' sx={{ mb: 1.5 }} color="text.secondary" style={{cursor: "pointer"}}>
+                {member.email}
+              </Typography>
+            </Grid>
+            {/* The manage button area */}
+            <Grid item>
+              <Stack direction="row" justifyContent="space-between" spacing={5}>
+                  <Button  sx={{ maxWidth: '110px', maxHeight: '40px', minWidth: '110px', minHeight: '40px'}}  size="medium" variant="contained">Manage</Button>
+              </Stack>
+            </Grid>
+          </Grid>
         </CardContent>
     </React.Fragment>
 );
