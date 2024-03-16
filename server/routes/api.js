bea410d3781c402d6a2fa77892de54bda3894bba
Jezerig <jesse_pasanen@hotmail.com>
2024-03-16 14:37:16 +0200

    Exclude password when fetching members

--- server/routes/api.js
+++ server/routes/api.js
@@ -22,7 +22,7 @@ router.use(passport.initialize());
 //finds all the members in the DB if authenticated
 router.get("/members/", passport.authenticate('jwt', {session: false}), async (req, res) => {
     try {
-      const members  = await Member.find({});
+      const members  = await Member.find({}).select("-password");
       res.send(members);
     } catch (err) {
       console.error('Error fetching member data:', err);
